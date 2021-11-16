045b8a916f4561da700792eea0b10c063ad0b540
Edvin <edvinture@mail.ru>
2021-11-16 10:10:13 +0200

    OS

---
+++ refs/of_ref.js
@@ -0,0 +1,22 @@
+const os = require ('os')
+
+//Platforma
+console.log(os.platform())
+
+//Arhitektura
+console.log(os.arch())
+
+//informaciya
+console.log(os.cpuc())
+
+// svobodnaya pamyat'
+console.log(os.freemem())
+
+// skolko vsego pamyati
+console.log(os.totalmem())
+
+// kornevaya direktoriya
+console.log(os.homedir())
+
+//skolko sistema rabotayet
+console.log(os.uptime())
